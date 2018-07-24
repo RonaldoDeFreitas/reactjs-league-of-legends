ed52f4025556760a76ce1039f15d713882ac96de
Ronaldo de Freitas <freitasderonaldo@gmail.com>
2018-07-24 13:02:23 -0300

    implementing menu

--- src/app/components/components/header/Header.js
+++ src/app/components/components/header/Header.js
@@ -8,6 +8,15 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import Divider from '@material-ui/core/Divider';
+
 
 
 const styles = {
@@ -21,17 +30,64 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  list: {
+    width: 250,
+  },
 };
 
 class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      left: false,
+    };
+  }
+
+
+  toggleDrawer(side, open) {
+    this.setState({
+      [side]: open,
+    });
+  };
+
   render() {
     const { classes } = this.props;
+
+    const sideList = (
+      <div className={classes.list}>
+        <List component="nav">
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List component="nav">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItem button component="a" href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItem>
+      </List>
+      </div>
+    );
+
     return (
       <div className={classes.root}>
       <AppBar position="static" color="default">
         <Toolbar>
-          
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+
+          <IconButton onClick={() => this.toggleDrawer('left', true)} className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="title" color="inherit">
@@ -40,6 +96,16 @@ class Header extends Component {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Drawer open={this.state.left} onClose={() => this.toggleDrawer('left', false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={() => this.toggleDrawer('left', false)}
+            onKeyDown={() => this.toggleDrawer('left', false)}
+          >
+            {sideList}
+          </div>
+        </Drawer>
     </div>
     )
   }
